7194800a652d61e65feaa543058ce6a9a77afde7
Deepak S S <deepak_sureshkumarsubhakumari@intuit.com>
2021-03-25 00:12:23 +0530

    intial commit

---
+++ src/main/java/com/tweetapp/TweetApplication.java
@@ -0,0 +1,23 @@
+package com.tweetapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.tweetapp.services.MainMenuService;
+
+/**
+ * Hello world!
+ *
+ */
+public class TweetApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TweetApplication.class);
+
+	public static void main(String[] args) {
+		try {
+			MainMenuService mainMenuService = new MainMenuService();
+			mainMenuService.menu();
+		} catch (Exception e) {
+			LOGGER.info("Application Terminated. please Restart");
+		}
+	}
+}
